74f305101bfc41557244b469e3ee10cca2c3cbc3
DgvDilshan <dgvdilshan@students.nsbm.ac.lk>
2026-01-15 23:31:17 +0530

    set image and about section are set app.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import Navbar from './components/layout/navbar.jsx'
 import Hero from './components/sections/Hero.jsx'
+import About from './components/sections/About.jsx'
 
 const App = () => {
   return (
-    <div className='min-h-screen bg-black'>
+    <div className='min-h-screen bg-black pb-[100vh]'>
       <Navbar/>
        <main>
           <Hero/>
+          <About />
        </main>
     </div>
   )
